b71bde358f5c824148596cda4c8235d80e794009
Miguel <miguellealw@gmail.com>
2021-05-14 00:21:40 -0500

    fixed src link in gallery image

--- pages/gallery/index.vue
+++ pages/gallery/index.vue
@@ -50,7 +50,7 @@
               v-for="image in gallery.images"
               class="image"
               :key="image.id"
-              :src="`https://res.cloudinary.com/miguelleal/image/uploadc_thumb,w_200,g_face/v1620969351${gallery.path}/01.jpg`"
+              :src="`https://res.cloudinary.com/miguelleal/image/uploadc_thumb,w_200,g_face/v1620969351${gallery.path}${image}`"
             />
           </div>
         </li>
