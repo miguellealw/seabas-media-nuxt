d200feaef16ba5f217ed0188c2e0e5d0366bea4a
Miguel <miguellealw@gmail.com>
2021-05-20 05:26:54 -0500

    changed footer to new site name

--- components/global/Footer.vue
+++ components/global/Footer.vue
@@ -1,10 +1,8 @@
 <template>
   <div>
     <hr class="w-1/2 mx-auto mt-6" />
-    <footer
-      class="w-full h-16 text-center uppercase text-gray-500 text-xs flex items-center justify-center"
-    >
-      Leal Media &#8482;
+    <footer class="w-full h-16 text-center uppercase text-gray-500 text-xs flex items-center justify-center">
+      Seabas Media &#8482;
     </footer>
   </div>
 </template>
