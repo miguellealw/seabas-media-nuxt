2b3ad4d6651e80a4ed4b2b366e561d2968c919b6
Miguel <miguellealw@gmail.com>
2021-05-23 22:38:54 -0500

    style fix: fixed gab b/w text and image in about me

--- components/home/HomeAboutMe.vue
+++ components/home/HomeAboutMe.vue
@@ -2,17 +2,21 @@
   <section id="about-me" class="second-fade-in w-full min-h-screen flex items-center">
     <!-- style="height: 25rem;" -->
     <!-- lg:ml-auto lg:mr-12 -->
-    <div class="w-3/4 lg:w-1/2 grid grid-rows-2 md:grid-rows-1 grid-cols-1 gap-10 md:grid-cols-2 mx-auto">
+    <div
+      class="w-3/4 lg:w-1/2 grid grid-rows-2 md:grid-rows-1 grid-cols-1 md:grid-cols-2 auto-rows-min md:gap-10 mx-auto"
+    >
       <!-- About Me content -->
-      <div class="row-span-1">
+      <div>
         <h3 class="text-sm font-bold uppercase my-6">About Me | Sebastian Leal</h3>
         <!-- <h4 class="text-sm uppercase"></h4> -->
         <p class="leading-7 text-sm">
           {{ text }}
         </p>
       </div>
+
       <!-- Profile Image -->
-      <div class="w-full mx-auto bg-orange-300" style="height: 30rem">
+      <!-- <div class="w-full mx-auto bg-orange-300" style="height: 30rem"> -->
+      <div class="w-full mx-auto bg-orange-300">
         <img :src="image" class="object-cover object-center w-full h-full" alt="" />
       </div>
     </div>
