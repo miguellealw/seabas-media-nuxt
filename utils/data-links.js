219bf404ff24cacf00c57900448f08eea1bba50b
Miguel <miguellealw@gmail.com>
2021-05-30 15:35:20 -0500

    feat: Video now embeds youtube videos

--- utils/data-links.js
+++ utils/data-links.js
@@ -4,28 +4,49 @@ const cloudName = 'seabas-media'
 const cloudVersion = 'v1621218000'
 
 function createVideoLink(link) {
+  // https://www.youtube.com/user/OW3NxX/videos
+
+  // <iframe width="560" height="315" src="https://www.youtube.com/embed/bmiS3wblRg0" title="YouTube video player"
+  //         frameBorder="0"
+  //         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+  //         allowFullScreen></iframe>
+
   const arr = link.split('/')
-  const vidNum = arr[arr.length - 1]
+  const vidID = arr[arr.length - 1]
 
   // e.g. convert https://vimeo.com/466777132 to https://player.vimeo.com/video/466777132
-  const videoLink = 'https://player.vimeo.com/video/' + vidNum
+  // return 'https://player.vimeo.com/video/' + vidID
 
-  return videoLink
+  // convert https://youtu.be/bmiS3wblRg0 to https://www.youtube.com/embed/bmiS3wblRg0
+  return `https://www.youtube.com/embed/${vidID}`
 }
 
 /**
  *
  * @param {string} path - path to image/video in cloudinary/vimeo (gallery.path + image)
- * @returns - object with link to thumnail and src
+ * @returns - object with link to thumbnail and src
  */
 export const createLink = (path, isImage = true) => {
   const mediaType = isImage ? 'image' : 'video'
 
+  // VIMEO
+  // if (mediaType === 'video') {
+  //   return {
+  //     src: `<iframe title="vimeo-player" src="${createVideoLink(
+  //       path
+  //     )}" width="640" height="360" frameborder="0" allowfullscreen class="vimeo-video"></iframe>`
+  //   }
+  // }
+
+  // YOUTUBE
   if (mediaType === 'video') {
     return {
-      src: `<iframe title="vimeo-player" src="${createVideoLink(
+      src: `<div class="videoWrapper" style="--aspect-ratio: 3/4;"><iframe width="640" height="360" src="${createVideoLink(
         path
-      )}" width="640" height="360" frameborder="0" allowfullscreen class="vimeo-video"></iframe>`
+      )}" title="YouTube video player"
+               frameBorder="0"
+               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+               allowFullScreen class="vimeo-video"></iframe></div>`
     }
   }
 
